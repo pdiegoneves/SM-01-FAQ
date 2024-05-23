5f9cf96d76aaa6d0bd2d50b7398cbcfc6e7c21f7
Diego Neves <pdcassiano@gmail.com>
2024-05-22 23:24:58 -0300

    feat: adicionamos as perguntas e respostas

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="FAQ - Streamlit",
+    page_icon="./img/logo.png",
+    layout="centered"
+)
+
+col1, col2 = st.columns([3, 9])
+
+col1.image("./img/logo.png")
+
+with col2:
+    st.title("FAQ - Streamlit")
+    st.caption("Frequently Asked Questions")
+
+st.header("Perguntas frequentes sobre o Streamlit")
+
+with st.expander("O que é Streamlit?"):
+    st.write('Streamlit é uma biblioteca de código aberto em Python que facilita a criação de aplicativos da web interativos para visualização de dados. Com Streamlit, você pode transformar scripts Python em aplicativos da web interativos, sem necessidade de conhecimento em desenvolvimento web.')
+
+with st.expander("Como testar o streamlit?"):
+    st.text("No terminal digite")
+    st.code("streamlit hello")
+
+with st.expander("Como rodar o streamlit?"):
+    st.text("No terminal digite")
+    st.code("streamlit run app.py")
+    st.caption("Onde app.py é o arquivo principal do Streamlit")
+
+with st.expander("Posso rodar o streamlit em outra porta?"):
+    st.success("Sim, basta passar a porta que deseja")
+    st.text("No terminal digite")
+    st.code("streamlit run app.py --server.port 8080")
+    st.caption("Onde app.py é o arquivo principal do Streamlit e 8080 é a porta")
+
+with st.expander("Como adicionar gráficos em um aplicativo Streamlit?"):
+    st.markdown("Você pode adicionar gráficos em um aplicativo Streamlit utilizando bibliotecas de visualização de dados como **Matplotlib, Seaborn e Plotly**.'")
